956a8be2cd49e2c2a79768ee0b8ba9d66484bce4
PomDetom <111182697+PomDetom@users.noreply.github.com>
2024-05-12 23:52:22 +0800

    模拟实现atoi()函数

---
+++ test-24-5-12/test-24-5-12/atoi.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int my_atoi(const char* str)
+{
+	int count_num = 0;
+	int count_str = 0;
+
+	if (*str == '+' || *str == '-')
+	{
+		count_str++;
+	}
+	while (*(str + count_str + count_num) >= '0' && *(str + count_str + count_num) <= '9')
+	{
+		count_num++;
+	}
+	if (count_num != 0)
+	{
+		int ret = 0;
+		int i = 0;
+		int j = 1;
+
+		for (i = 0; i < count_num; i++)
+		{
+			ret += (*(str + count_num + count_str - i - 1) - 48) * j;
+			j *= 10;
+		}
+		if (*str == '-')
+		{
+			return ((-1) * ret);
+		}
+		else
+		{
+			return ret;
+		}
+	}
+	
+	return 0;
+}
+
+int main()
+{
+	//模拟实现atoi函数
+	char arr[100] = { "-341234.567" };
+
+	long ret = my_atoi(arr);
+
+	printf("%ld\n", ret);
+
+	return 0;
+}
