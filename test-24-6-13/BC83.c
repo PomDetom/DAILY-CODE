1771efd48518c50fa9ab853bff100e2761011e82
PomDetom <111182697+PomDetom@users.noreply.github.com>
2024-06-13 23:53:03 +0800

    BC83 牛牛学数列

---
+++ test-24-6-13/BC83.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//BC83 Å£Å£Ñ§ÊýÁÐ
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    int num = 0;
+    int i = 0;
+
+    for (i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            num += (-1) * i;
+        }
+        else
+        {
+            num += i;
+        }
+    }
+    printf("%d", num);
+    
+    return 0;
+}
