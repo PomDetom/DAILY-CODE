5afc390ae13c9541e486f2b7015491efd529f456
PomDetom <111182697+PomDetom@users.noreply.github.com>
2024-06-14 23:15:42 +0800

    BC84 牛牛学数列2

---
+++ test-24-6-14/BC84.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//BC84 Å£Å£Ñ§ÊýÁÐ2
+int main() 
+{
+    int N = 0;
+    scanf("%d", &N);
+    double sum = 0;
+    int i = 0;
+
+    for (i = 1; i <= N; i++)
+    {
+        double num = 1.0 / i;
+        sum += num;
+    }
+    printf("%.6f", sum);
+    
+    return 0;
+}
